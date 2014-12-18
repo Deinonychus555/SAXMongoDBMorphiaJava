5bf3a6d0cdc058daa49b54cce12ed743a3a67af0
Echeverrias <juanan.echeve@gmail.com>
2014-12-18 18:06:51 +0100

    añadido titulo a la clase Articulo

--- src/Models/Article.java
+++ src/Models/Article.java
@@ -17,24 +17,59 @@ import com.mongodb.BasicDBObject;
  * @author Oscar Miranda Bravo
  */
 public class Article extends BasicDBObject{
-    
+
+       
     private String key;
     
     private String authors[];
     
+    private String title;
+  
     private int year;
     
     private String journal;
     
-    
     public Article(){};
+       
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String[] authors) {
+        this.authors = authors;
+    }
     
-    public Article (String key, String authors[], int year, String journal){
-        
-        this.key=key;
-        this.year=year;
-        this.journal=journal;
-        System.arraycopy(authors, 0, this.authors, 0, authors.length);
-    };
+     public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getJournal() {
+        return journal;
+    }
+
+    public void setJournal(String journal) {
+        this.journal = journal;
+    }
+ 
     
 }
